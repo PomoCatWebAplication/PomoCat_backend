2f5d44e9e8ad33ebd4f1bb832e28c3fed6d0f916
Yura <yurayt18@gmail.com>
2025-09-08 21:44:46 -0500

    pequeño error en pet

--- src/modules/pet/pet.module.ts
+++ src/modules/pet/pet.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PetService } from './pet.service';
 import { PetController } from './pet.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PetSchema } from './schemas/pet.schema';
 
 @Module({
   imports: [

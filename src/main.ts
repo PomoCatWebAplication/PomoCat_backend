d7f16a6243765fadb4bb930685643a9c6888e91d
Yura <yurayt18@gmail.com>
2025-09-30 16:38:11 -0500

    cambio de puerto

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -15,6 +13,12 @@ async function bootstrap() {
       },
     })
   );
+  app.enableCors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 4000);
 
 }
 bootstrap();
